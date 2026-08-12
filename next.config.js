df1c12ddfd73472adfd00804308e2834453bf223
UniNahn <nathanjamescatapang022@gmail.com>
2026-08-12 20:56:21 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'export',
+  basePath: '/Catapang_Portfolio',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
